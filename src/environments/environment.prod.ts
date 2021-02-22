37db8559db178a9b6e7bcd5abeaeef6ccf7eb66d
Eric Zawadski <ericz@live.ca>
2021-02-22 09:27:50 -0500

    deployed with firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA9ipDmQB_BjjwBDR39rzYMqGqryM2Xuww',
+    authDomain: 'fir-course-9584f.firebaseapp.com',
+    projectId: 'fir-course-9584f',
+    storageBucket: 'fir-course-9584f.appspot.com',
+    messagingSenderId: '44396963243',
+  },
 };
